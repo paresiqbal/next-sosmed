7b6770b0fa74bc1fa2155399ecb66de84f8d6ca8
paresiqbal <thecrowdel@gmail.com>
2023-01-20 21:10:08 +0700

    fix

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,11 +1,14 @@
 export default function Message({ children, avatar, username, description }) {
   return (
-    <div className="container bg-primary-green p-8 border-b-2 rounded-lg items-center shadow-md">
-      <div className="flex ">
-        <img src={avatar} className="w-9 rounded-full" />
-        <h2 className="text-xl font-medium px-2">{username}</h2>
+    <div className="bg-secondary-blue p-6 border-b-2 rounded-md">
+      <div className="flex items-center gap-2">
+        <img src={avatar} className="w-10 rounded-full" />
+        <div className="flex flex-col">
+          <h2 className="text-md">{username}</h2>
+          <p className="text-xs">7:20 PM · Jan 19, 2023</p>
+        </div>
       </div>
-      <div className="py-4">
+      <div className="py-5">
         <p>{description}</p>
       </div>
       {children}
