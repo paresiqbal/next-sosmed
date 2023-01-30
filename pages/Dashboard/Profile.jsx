19d64c8b0583e025a900a4e289941fbc6ff0243c
paresiqbal <thecrowdel@gmail.com>
2023-01-30 11:37:14 +0700

    Adding profile

--- pages/Dashboard/Profile.jsx
+++ pages/Dashboard/Profile.jsx
@@ -3,15 +3,12 @@ import Link from "next/link";
 import { auth } from "@/utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-export default function Profile() {
+export default function Navbar() {
   const [user, loading] = useAuthState(auth);
+  console.log(user);
 
   return (
     <div className="p-5 shadow-md text-xl flex flex-col items-center gap-2 lg:flex lg:flex-row lg:justify-between">
-      <div className="flex gap-4">
-        <Link href="/">Home</Link>
-        <Link href="/Dashboard/Blog">Blog</Link>
-      </div>
       <div>
         {!user && ( //Checking user login or not
           <Link
@@ -21,36 +18,16 @@ export default function Profile() {
             Login
           </Link>
         )}
-        {user && ( //Change login to post if user already login
-          <div className="flex items-center gap-4">
-            <Link
-              href="/Dashboard/Post"
-              className="font-semibold text-base bg-primary-green px-4 py-2 rounded-md"
-            >
-              Post
-            </Link>
-            <Link
-              href="/Dashboard/PostImage"
-              className="font-semibold text-base bg-primary-green px-4 py-2 rounded-md"
-            >
-              Post Image
-            </Link>
-            <Link
-              href="/Dashboard/ChatRoom"
-              className="font-semibold text-base bg-primary-green px-4 py-2 rounded-md"
-            >
-              Chat
-            </Link>
-            <Link
-              href="/Dashboard/User"
-              className="lg:flex items-center align-middle gap-1"
-            >
-              <p className="text-sm ">{user.displayName}</p>
-              <img
-                src={user.photoURL}
-                className="w-9 rounded-full cursor-pointer"
-              />
-            </Link>
+        {user && (
+          <div>
+            <div className="flex flex-col items-center gap-2">
+              <img className="rounded-full w-25" src={user.photoURL} />
+              <h1 className="font-semibold underline decoration-purple-500">
+                {user.displayName}
+              </h1>
+              <p className="text-sm">{user.email}</p>
+            </div>
+            <div>Creat at</div>
           </div>
         )}
       </div>
